5019425e097accd4273a9fb103b50b241f41d15b
Alexandre Tavares <alexandre.j.tavares.jr@gmail.com>
2024-02-06 01:35:45 -0300

    fix: "Ajustando erro de charset na URI de conexão com banco de dados"

--- core/database.py
+++ core/database.py
@@ -1,8 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from .settings import get_settings
+import os
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/Project.env"
+SQLALCHEMY_DATABASE_URL = get_settings().db_url
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
--- src/projects/schemas.py
+++ src/projects/schemas.py
@@ -15,4 +15,4 @@ class ProjectResponse(Project):
     id: int
 
     class Config:
-        orm_mode = True
+        from_attributes = True
